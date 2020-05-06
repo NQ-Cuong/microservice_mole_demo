7041d5f724e059e33fcfa261c7986ca4b9a5b457
cuongnq <nguyenquoccuong290197@gmail.com>
2020-05-06 22:15:41 +0700

    commit 1

---
+++ packages/backend/services/users/user.service.js
@@ -0,0 +1,26 @@
+const { SERVICE_USER, TABLE_USER } = require('../../constants');
+
+module.exports = {
+  name: SERVICE_USER,
+  mixins: [],
+  actions: {
+    getAllUsers: {
+      cache: {
+        keys: ['active'],
+        ttl: 10,
+      },
+      params: {
+        active: {
+          type: 'string',
+          optional: true,
+        },
+      },
+      handler(ctx) {
+        const { active } = ctx.params;
+        return ctx.call(`db-${TABLE_USER}.find`, {
+          where: { actFlg: active === 'true' },
+        });
+      },
+    },
+  },
+};
